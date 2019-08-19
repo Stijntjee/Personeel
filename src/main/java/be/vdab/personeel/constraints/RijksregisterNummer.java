cfaaafacaa343d46b42a07336ae81f73c34b7bb0
Stijn Buys <47979587+Stijntjee@users.noreply.github.com>
2019-08-19 15:53:39 +0200

    commitj

---
+++ src/main/java/be/vdab/personeel/constraints/RijksregisterNummer.java
@@ -0,0 +1,20 @@
+package be.vdab.personeel.constraints;
+
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Retention(RUNTIME)
+@Target({FIELD, METHOD, ANNOTATION_TYPE, })
+@Constraint(validatedBy = RijksregisterNummerValidator.class)
+public @interface RijksregisterNummer
+{
+    String message() default "{be.vdab.personeel.constraints.RijksregisterNummer.message}";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
